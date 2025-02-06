ca94a942aa8450ac22ae1bcd4107754183856469
rayyan22207 <rayyan22207@gmail.com>
2025-02-06 20:09:08 +0500

    working on

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-@login_required
 def dashboard_view(request):
     return render(request, "dashboard/main.html", {})
