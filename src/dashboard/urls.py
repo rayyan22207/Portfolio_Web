31b08a4142b11743122935e7bb8d04a97b964382
rayyan22207 <rayyan22207@gmail.com>
2025-02-12 19:35:33 +0500

    working on

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard_view, name='home')
+    path('', views.dashboard_view, name='home'),
+    path('projects/', views.projects, name='projects')
 ]
--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import Project
+
+def projects(request):
+    all_projects = Project.objects.all()
+    return render(request, 'dashboard/projects.html', {'projects': all_projects})
+
 
 def dashboard_view(request):
     return render(request, "dashboard/main.html", {})
